46a16783f048f734bd9d3410e935bac7ac51d3b1
Floris <munezerofloris@gmail.com>
2025-04-26 00:28:03 +0200

    headerBar

---
+++ flosocial/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Bars from '../../components/bars/Bars';
+
+const Home = () => {
+    return (
+        <div>
+           <Bars/> 
+        </div>
+    );
+};
+
+export default Home;
---
+++ flosocial/src/components/bars/Bars.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+const Bars = () => {
+    return (
+        
+            <div className="header_bar">
+                <div className="logo">
+                    <h1>FloSocail <i class="fa-solid fa-people-arrows connect"></i></h1>
+                </div>
+                <div className="search">
+                <i class="fa-solid fa-magnifying-glass user_search"></i>
+                    <input placeholder='search for freinds_video_content' className="inputSearch" />
+                </div>
+
+                <div className="chat">
+                    <div className="user">
+                    <i class="fa-solid fa-user"></i>
+                    <span className="user_span">
+                        1
+                    </span>
+                    </div>
+                    
+                    <div className="notification">
+                    <i class="fa-solid fa-square-envelope"></i>
+                    <span className="notification_span">
+                        1
+                    </span>
+                    </div>
+
+                    <div className="message">
+                    <i class="fa-solid fa-message"></i>
+                    <span className="sms">
+                        2
+                    </span>
+                    </div>
+
+                    <div className="profile">
+                        <img src="./assets/profile.jpg" alt="" />
+                    </div>
+
+                </div>
+            </div>
+        
+    );
+};
+
+export default Bars;
