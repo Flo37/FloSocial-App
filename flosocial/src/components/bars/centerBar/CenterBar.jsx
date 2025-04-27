7fffb4f41c94e29136ed65910482f44c479907de
Floris <munezerofloris@gmail.com>
2025-04-27 20:34:16 +0200

    centerbar_share

--- flosocial/src/components/bars/centerBar/CenterBar.jsx
+++ flosocial/src/components/bars/centerBar/CenterBar.jsx
@@ -5,7 +5,15 @@ const CenterBar = () => {
         <div className='centerBar'>
             <div className="container">
             <img src="./Assets/profile.jpg" alt="" />
-            <span>hello</span>
+            <input placeholder="What's do you think?"/>
+            <hr />
+            <div className="icons">
+            <span className='media' ><i class="fa-solid fa-photo-film"></i> photo or video</span>
+            <span className='tag'><i class="fa-solid fa-tag"></i> tag</span>
+            <span className='location'><i class="fa-solid fa-location-dot"></i> location</span>
+            <span className='feelings'><i class="fa-solid fa-face-smile"></i> feelings</span>
+            <button  className='share'>Share</button>
+            </div>
             </div>
         </div>
     );
